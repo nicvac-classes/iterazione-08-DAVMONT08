64faa22e19ff0887fe61430b3451130afddc5fc6
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-10-11 22:47:43 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,48 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+              int i,b,p,s,n,vm;
+              string nome;  
+              i=0;
+              p=0;
+              b=0;
+              s=0;
+              do
+              {
+                  cout<<"quanti studenti ci sono in classe?"<<endl;
+                  cin>> n;
+                  }
+               while(n<0)
+
+               while(i<n)
+               {
+                  cout<<"qual'è il tuo nome?"<<endl;
+                  cin>> nome;
+                  cout<< "quale è il tuo voto medio in informatica?"<<endl;
+                  cin>> vm;
+                  if(vm>=6)
+                  {
+                     p=p+1;
+                     }
+                  {
+                     b=b+1;
+                     }
+                     s=s+vm;
+                     i=i+1 ;
+                     }
+                  {
+                      vm=s/n;
+                      cout<<"prenderanno il debito" <<b << "saranno promossi" <<p <<endl;
+                      cout<<"il voto medio è" <<vm;
+                      }
+
+
+
+                      
+                    }
+
+
+
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
